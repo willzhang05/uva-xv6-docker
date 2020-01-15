fe0ffcadc74508362b7c24f5d008a3d4802c9546
William Zhang <willzhang05@gmail.com>
2020-01-15 16:02:05 -0500

    fix run.sh

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,4 @@
 FROM ubuntu:18.04
 RUN apt-get update && apt-get -y install git make gcc perl-base qemu-system-i386 && apt-get clean
-#git clone https://github.com/uva-reiss-cs4414/xv6.git
-ADD run.sh .
+ADD run.sh /
 ENTRYPOINT ["/run.sh"]
--- run.sh
+++ run.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 
-if [ -d "/xv6" ]; then
+if [ -f "/xv6/Makefile" ]; then
     cd /xv6
     /usr/bin/make qemu-nox
 else
     echo "Please make sure to run with the bind mount to your copy of the xv6 source."
-    /bin/sh
 fi
-        
